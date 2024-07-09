455373d3544f15bc6b6650fffcfceef8d2641c41
GURUCle <kameetac@gmail.com>
2024-07-09 02:32:41 +0200

    we have added sign in capabilities wth json webtoken that expires too, remove password from client res.

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../model/user.model.js";
 import bcrypt from 'bcrypt';
 import { errorHandler } from "../routes/utils/error.js";
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
 
 const saltRounds = 10;
 
@@ -28,3 +30,23 @@ export const signup = async (req, res, next) => {
         next(error);
     }
 };
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    try {
+const validUser = await User.findOne({email});
+if (!validUser) return next(errorHandler(404, 'user credential invalid'));
+
+const validPassword = await bcrypt.compareSync(password,validUser.password);
+if (!validPassword) return next(errorHandler(401, 'user credential invalid'));
+const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET,{ expiresIn: 60 * 60 });
+const {password: hashedPassword, ...rest} = validUser._doc;
+res
+.cookie('access_token', token, {httpOnly:true})
+.status(200)
+.json(rest)
+    }catch(error) {
+        next(error)
+    }
+}
